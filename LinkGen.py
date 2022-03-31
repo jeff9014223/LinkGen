79a26080cd43903679d3de0240162c0dc999453f
Snikker#6211 <99515656+Snikkertjuh@users.noreply.github.com>
2022-03-31 14:06:59 +0200

    Removed Premium

--- LinkGen.py
+++ LinkGen.py
@@ -44,6 +44,11 @@ try:
 except Exception:
     print("[ERROR] Guild ID not set")
     input()
+try:
+    json.loads(open("config.json", "r").read())["log_channel"]
+except Exception:
+    print("[ERROR] Log Channel not set")
+    input()
 
 @client.event
 async def on_ready():
@@ -52,14 +57,9 @@ async def on_ready():
     print("LinkGen Ready\n")
     await client.change_presence(activity=discord.Game(name="LinkGen V2.0"))
     try:
-        client.guilds[0].get_role(int(json.loads(open("config.json", "r").read())["free_role"]))
+        client.guilds[0].get_role(int(json.loads(open("config.json", "r").read())["gen_role"]))
     except Exception:
-        print("[ERROR] Free Plan not set")
-        input()
-    try:
-        client.guilds[0].get_role(int(json.loads(open("config.json", "r").read())["premium_role"]))
-    except Exception:
-        print("[ERROR] Premium Plan not set")
+        print("[ERROR] Gen Role not set")
         input()
     try:
         client.guilds[0].get_channel(int(json.loads(open("config.json", "r").read())["gen_channel"]))
@@ -79,10 +79,10 @@ async def generate(ctx, service_name):
     if str(ctx.channel.id) != json.loads(open("config.json", "r").read())["gen_channel"]:
         await ctx.respond(f"You can only gen in: <#{json.loads(open('config.json', 'r').read())['gen_channel']}>", ephemeral=True)
     else:
-        free_services = ["NordVPN", "Hulu", "ExpressVPN", "Nitro", "CreditCard"]
-        for service in free_services:
+        services = ["NordVPN", "Hulu", "ExpressVPN", "Nitro", "CreditCard", "Spotify", "Netflix", "Disney", "Minecraft"]
+        for service in services:
             if service_name.lower() == service.lower():
-                if str(json.loads(open("config.json", "r").read())["free_role"]) in str(ctx.author.roles):
+                if str(json.loads(open("config.json", "r").read())["gen_role"]) in str(ctx.author.roles):
                     if os.path.exists(f"accounts/{service}.txt"):
                         with open(f"accounts/{service}.txt", "r+") as accounts:
                             data = accounts.readlines()
@@ -95,42 +95,9 @@ async def generate(ctx, service_name):
                                 embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/773133136929226763/797204521997828106/777514274829893683.gif")
                                 user = await client.fetch_user(int(ctx.author.id))
                                 await user.send(embed=embed)
-                                open(f"logs/{service}.txt", "a").write(f"""
-
-                                Generated: {service} by: {ctx.author.name}
-                                Account: {data[0]}
-
-                                """)
-                                await ctx.respond("Account Generated, check your DM")
-                            except Exception:
-                                await ctx.respond(f"We are currently out of {service}!", ephemeral=True)
-                    else:
-                        await ctx.respond(f"We are currently out of {service}!", ephemeral=True)
-                else:
-                    await ctx.respond(f"You cannot gen {service}!", ephemeral=True)
-        premium_services = ["Spotify", "Netflix", "Disney", "Minecraft"]
-        for service in premium_services:
-            if service_name.lower() == service.lower():
-                if str(json.loads(open("config.json", "r").read())["premium_role"]) in str(ctx.author.roles):
-                    if os.path.exists(f"accounts/{service}.txt"):
-                        with open(f"accounts/{service}.txt", "r+") as accounts:
-                            data = accounts.readlines()
-                            accounts.seek(0)
-                            accounts.truncate()
-                            accounts.writelines(data[1:])
-                            try:
-                                embed = discord.Embed(title=f"{service} Account Generated", description="LinkGen Account Generator", color=0x46a9f0)
-                                embed.add_field(name="Login Credentials", value=f"```{data[0]}```", inline=True)
-                                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/773133136929226763/797204521997828106/777514274829893683.gif")
-                                embed.set_footer(text="Made by Snikker#1337")
-                                user = await client.fetch_user(int(ctx.author.id))
-                                await user.send(embed=embed)
-                                open(f"logs/{service}.txt", "a").write(f"""
-
-                                Generated: {service} by: {ctx.author.name}
-                                Account: {data[0]}
-
-                                """)
+                                log = client.guilds[0].get_channel(int(json.loads(open("config.json", "r").read())["log_channel"]))
+                                embed = discord.Embed(title=f"{ctx.author.name} has genned 1 {service}", description=f"**Account**\n```{data[0]}```", color=0x46a9f0)
+                                await log.send(embed=embed)
                                 await ctx.respond("Account Generated, check your DM")
                             except Exception:
                                 await ctx.respond(f"We are currently out of {service}!", ephemeral=True)
@@ -142,8 +109,7 @@ async def generate(ctx, service_name):
 @client.slash_command(name="help", guild_ids=[json.loads(open("config.json", "r").read())["guild_id"]])
 async def help(ctx):
     embed = discord.Embed(title="LinkGen help command", description="Usage: /generate <service name>, /stock", color=0x46a9f0)
-    embed.add_field(name="Free Plan", value="``nordvpn``, ``hulu``, ``expressvpn``, ``nitro``, ``creditcard``")
-    embed.add_field(name="Premium Plan", value="``spotify``, ``netflix``, ``disney``, `minecraft`")
+    embed.add_field(name="All Services", value="``nordvpn``, ``hulu``, ``expressvpn``, ``nitro``, ``creditcard``, ``spotify``, ``netflix``, ``disney``, ``minecraft``")
     embed.set_footer(text="Made by Snikker#1337")
     await ctx.respond(embed=embed)
 
